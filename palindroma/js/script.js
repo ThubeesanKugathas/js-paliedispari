198c8c4287dc872e1d62419992e1d633a9174e34
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-16 18:41:33 +0100

    comments js

---
+++ palindroma/js/script.js
@@ -0,0 +1,5 @@
+// creare funzione che possa comprendere se la parola inserita dall'utente sia palindroma
+// es. parola inserita va divisa in unità (come array) e verificare se il suo inverso è lo stesso
+
+
+// chiedere all'utente la parola e verificare uguaglianza tra la forma originale ed invertita
---
+++ pari e dispari/js/script.js
@@ -0,0 +1,13 @@
+// creare una funzione che randomizza un numero per la macchina da 1 a 5
+
+// creare una funzione che verifichi la somma del numero scelto dall'utente e dal randomizer
+// della macchina sia pari o dispari
+
+
+
+// chiedere all'utente se vuole proporre pari o dispari
+
+// chiedere all'utente un numero da 1 a 5
+
+
+// a seconda della scelta dell'utente e del risultato somma dei due numeri, decretare il vincitore
